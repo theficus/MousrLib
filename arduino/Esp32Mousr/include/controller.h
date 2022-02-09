c0b43c89ba8f316f428c14255ec48f0919bfc81d
Adam Meltzer <adam@theficus.com>
2022-02-08 22:51:43 -0800

    Just checking in what I've got

--- arduino/Esp32Mousr/include/controller.h
+++ arduino/Esp32Mousr/include/controller.h
@@ -3,12 +3,12 @@
 #define MOUSR_CONTROLLER_H
 
 #include "common.h"
+#include <functional>
 
 #ifdef ARDUINO
 #include <Wire.h>
 
 // seesaw includes
-#include <Adafruit_ST7735.h> // Hardware-specific library
 #include <Adafruit_seesaw.h>
 
 // Seesaw
@@ -20,15 +20,56 @@
 #define JOYSTICK_H 2
 #define JOYSTICK_V 3
 
-// Calibration values
-#define JOYSTICK_H_CORRECTION 2
-#define JOYSTICK_V_CORRECTION 0
+enum class ButtonPress
+{
+    None = 0,
+    A = 1,
+    B = 1 << 1,
+    X = 1 << 2,
+    Y = 1 << 3,
+    Select = 1 << 4
+};
 
-#define JOY_IRQ_PIN 14 // ESP32
+struct AnalogStickMovement
+{
+    float angle;
+    float velocity;
+    bool isCentered;
+};
 
-bool setupSeesaw();
-void onButtonPress();
-void readSeesaw();
+typedef std::function<void(ButtonPress press)> controller_button_press_callback;
+typedef std::function<void(AnalogStickMovement move)> controller_analog_stick_move_callback;
+
+class Controller
+{
+public:
+    Controller(uint8_t irqPin);
+    ~Controller();
+
+    void begin(controller_button_press_callback buttonCallback, controller_analog_stick_move_callback stickCallback);
+    void end();
+    void calibrate();
+
+private:
+    bool giveSemaphore();
+    bool takeSemaphore();
+    void IRAM_ATTR onButtonPress();
+    void buttonPressTask(void *);
+    void analogStickTask(void *);
+
+    bool isFinalizing;
+    uint8_t irqPin;
+    Adafruit_seesaw ss;
+    int16_t x_ctr, y_ctr;
+    SemaphoreHandle_t sem;
+    QueueHandle_t buttonQueue;
+};
+
+/*
+let radian = atan2f(-self.xAxis.value, self.yAxis.value)
+        print("y=\(self.yAxis.value) x=\(self.xAxis.value) rad=\(radian) angle=\(radian * Float(180 / Float.pi))")
+        let angle = -radian * Float(180 / Float.pi)
+*/
 
 #endif // ARDUINO
 #endif // MOUSR_CONTROLLER_H
--- arduino/Esp32Mousr/src/controller.cpp
+++ arduino/Esp32Mousr/src/controller.cpp
@@ -1,62 +1,78 @@
 #include "controller.h"
 
-static uint32_t s_button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
-                       (1 << BUTTON_LEFT) | (1 << BUTTON_UP) |
-                       (1 << BUTTON_SEL);
+uint32_t s_button_mask =
+    (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
+    (1 << BUTTON_LEFT) | (1 << BUTTON_UP) |
+    (1 << BUTTON_SEL);
 
-static Adafruit_seesaw ss;
-
-bool setupSeesaw()
+Controller::Controller(uint8_t irqPin)
 {
-    if (ss.begin(0x49) == false)
-    {
-        s_println("Could not set up JoyWing!");
-        return false;
-    }
-
-    s_println("seesaw started");
-    s_print("version: ");
-    s_printlnb(ss.getVersion(), HEX);
-    s_print("options: ");
-    s_printlnb(ss.getOptions(), HEX);
-    
-    ss.pinModeBulk(s_button_mask, INPUT_PULLUP);
-    ss.setGPIOInterrupts(s_button_mask, true);
-    pinMode(JOY_IRQ_PIN, INPUT);
-    attachInterrupt(JOY_IRQ_PIN, onButtonPress, FALLING);
-    return true;
+    this->irqPin = irqPin;
+    this->sem = xSemaphoreCreateBinary();
+    this->giveSemaphore();
 }
 
-void onButtonPress()
+void IRAM_ATTR Controller::onButtonPress()
 {
-    s_print("Read: ");
-    s_printlnb(ss.digitalReadBulk(s_button_mask), BIN);
-    //readSeesaw();
+    uint8_t v = 0;
+    xQueueSendFromISR(buttonQueue, &v, NULL);
 }
 
-void readSeesaw()
+void Controller::buttonPressTask(void *)
 {
-    /*
-    if (digitalRead(JOY_IRQ_PIN) == true)
+    s_println("buttonPressTask() enter");
+    uint32_t lastValue = 0;
+    while (this->isFinalizing == false)
     {
-        // Serial.println("JOY_IRQ_PIN READ");
-        return;
+        void *p;
+        if (xQueueReceive(buttonQueue, &p, 10000000) == false)
+        {
+            continue;
+        }
+
+        xSemaphoreTake(this->sem, portMAX_DELAY);
+        uint32_t v = this->ss.digitalReadBulk(s_button_mask);
+        xSemaphoreGive(this->sem);
+
+        if (lastValue != v)
+        {
+            lastValue = v;
+            ButtonPress bp = {};
+
+        }
     }
-    */
 
-    // Transform from 0...1024 to -128...127
-    int x = (ss.analogRead(JOYSTICK_H) / 4 - 128) + JOYSTICK_H_CORRECTION;
-    int y = (ss.analogRead(JOYSTICK_V) / 4 - 128) + JOYSTICK_V_CORRECTION;
+    vTaskDelete(NULL);
+}
 
-    uint32_t buttons = ss.digitalReadBulk(s_button_mask);
-    s_printf("Analog: X: %d Y: %d\n", x, y);
-    s_printf("Button: A: %d B: %d Y: %d X: %d SEL: %d\n",
-                  buttons & (1 << BUTTON_RIGHT),
-                  buttons & (1 << BUTTON_DOWN),
-                  buttons & (1 << BUTTON_LEFT),
-                  buttons & (1 << BUTTON_UP),
-                  buttons & (1 << BUTTON_SEL));
+void Controller::analogStickTask(void *)
+{
+}
 
-    s_print("Button (mask):");
-    s_printlnb(buttons, BIN);
+bool Controller::giveSemaphore()
+{
+    return xSemaphoreGive(this->sem);
+}
+
+bool Controller::takeSemaphore()
+{
+    return xSemaphoreTake(this->sem, portMAX_DELAY);
+}
+
+ButtonPress getPressedButtons(uint32_t mask)
+{
+    ButtonPress bp = {};
+    if (!(mask & (1 << BUTTON_RIGHT)))
+    {
+        bp |= (uint32_t)ButtonPress::A;
+    }
+
+    if (!(mask & (1 << BUTTON_DOWN)))
+    {
+
+    }
+}
+
+void calculatePosition(int16_t x, int16_t y, float &angle, float &velocity)
+{
 }
